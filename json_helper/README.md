a72ed6ad96b1d1d033da8916ff0abcab56ee7517
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-21 17:03:33 -0400

    First Upload

---
+++ json_helper/README.md
@@ -0,0 +1,4 @@
+# asamrajsontopandas
+
+This is a simple package that converts json files to be read into pandas and create
+data frames.
---
+++ json_helper/json_helper/json_to_pandas.py
@@ -0,0 +1,20 @@
+import json
+
+def read_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def read_all_json_files(JSON_ROOT):
+    for direpath, direnames, filenames in os.walk(JSON_ROOT):
+        result = []
+        for f in filenames:
+            if f.endswith('.json'):
+                json_content_2 = read_json(os.path.join(JSON_ROOT,f))
+                for i in json_content_2["results"]:
+                    i["source"] = f
+                    result.append(i)
+        df = pd.DataFrame(result)
+        return df
+
